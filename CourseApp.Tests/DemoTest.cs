a5111a5c61ac7dcb5e52d90b2caee52c9af6a12b
Nikrybkin <nik070702@gmail.com>
2021-09-24 22:54:05 +0300

    without tests

--- CourseApp.Tests/DemoTest.cs
+++ CourseApp.Tests/DemoTest.cs
@@ -5,9 +5,58 @@ namespace CourseApp.Tests
     public class DemoTest
     {
         [Fact]
-        public void Test1()
+        public void Test()
         {
             Assert.True(true);
+        }
+
+        [Fact]
+        public void TestIntSum()
+        {
+            int firstNumber = 2;
+            int secondNumber = 3;
+            int expected = 5;
+            var calc = new Calculator();
+            var actual = calc.GetSum(firstNumber, secondNumber);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestIntProduct()
+        {
+            int firstNumber = 2;
+            int secondNumber = 3;
+            int expected = 6;
+            var calc = new Calculator();
+            var actual = calc.GetProduct(firstNumber, secondNumber);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestDoubleQuotient()
+        {
+            double firstNumber = 5;
+            double secondNumber = 2;
+            double expected = 2.5;
+            var calc = new Calculator();
+            var actual = calc.GetQuotient(firstNumber, secondNumber);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestDoubleQuotientNull()
+        {
+            var calc = new Calculator();
+            double firstNumber = 5;
+            double secondNumber = 0;
+            double expected = double.PositiveInfinity;
+
+            var actual = calc.GetQuotient(firstNumber, secondNumber);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
