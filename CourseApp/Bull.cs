93ffe50f251b84f45838395de49b23ec1923e189
Nikrybkin <71150246+Nikrybkin@users.noreply.github.com>
2021-12-07 21:53:21 +0300

    Add files via upload

---
+++ CourseApp/Bull.cs
@@ -0,0 +1,44 @@
+﻿namespace CourseApp
+{
+    using System;
+    /* using System.Collections.Generic;*/
+
+    public class Bull : Animals
+    {
+        private Random random = new Random();
+
+        public Bull()
+        : base()
+        {
+        }
+
+        public Bull(string name, int weight)
+        : base(name, weight)
+        {
+        }
+
+        public Bull(string name, int weight, int lard)
+        : base(name, weight, lard)
+        {
+        }
+
+        public Bull(string name, int weight, int age, int lard)
+        : base(name, weight, age, lard)
+        {
+        }
+
+        public override string Died()
+        {
+            int lard = Lard;
+            Lard = 0;
+            return $"{Name} убит\nПолучено {lard} сала\n";
+        }
+
+        public override string MakePhrase(string[] phraseArray)
+        {
+            int index = random.Next(0, 3);
+            string phrase = phraseArray[index];
+            return $"{phrase}";
+        }
+    }
+}
---
+++ CourseApp/Pig.cs
@@ -0,0 +1,49 @@
+﻿namespace CourseApp
+{
+    using System;
+    /*using System.Collections.Generic;*/
+
+    public class Pig : Animals
+    {
+        private Random random = new Random();
+
+        public Pig()
+        : base()
+        {
+        }
+
+        public Pig(string name, int weight)
+        : base(name, weight)
+        {
+        }
+
+        public Pig(string name, int weight, int lard)
+        : base(name, weight, lard)
+        {
+        }
+
+        public Pig(string name, int weight, int age, int lard)
+        : base(name, weight, age, lard)
+        {
+        }
+
+        public void AddLard(int lard)
+        {
+            Lard += lard;
+        }
+
+        public override string Died()
+        {
+            int lard = Lard;
+            Lard = 0;
+            return $"{Name} зарезана\nПолучено {lard} сала\n";
+        }
+
+        public override string MakePhrase(string[] phraseArray)
+        {
+            int index = random.Next(0, 3);
+            string phrase = phraseArray[index];
+            return $"{phrase}";
+        }
+    }
+}
