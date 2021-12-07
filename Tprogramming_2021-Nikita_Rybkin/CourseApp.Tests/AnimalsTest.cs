bfbc072c149236b9c52507337096dc84a08b2beb
Nikrybkin <71150246+Nikrybkin@users.noreply.github.com>
2021-12-07 21:52:03 +0300

    Add files via upload

---
+++ Tprogramming_2021-Nikita_Rybkin/CourseApp.Tests/AnimalsTest.cs
@@ -0,0 +1,54 @@
+﻿namespace CourseApp.Tests
+{
+    using Xunit;
+
+    public class AnimalsTest
+    {
+        [Theory]
+        [InlineData("Animals", "Animals")]
+        [InlineData("Бычок", "Бычок")]
+        public void TestName(string a, string exp)
+        {
+            Pig actualResult = new Pig(a, 1);
+            Assert.Equal(exp, actualResult.Name);
+        }
+
+        [Theory]
+        [InlineData(88, 88)]
+        [InlineData(-20, 0)]
+        public void TestWeight(int a, int exp)
+        {
+            Pig actual = new Pig(" ", a);
+            Assert.Equal(exp, actual.Weight);
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(-1, 0)]
+        [InlineData(88, 88)]
+        public void TestFat(int a, int expected)
+        {
+            Pig actual = new Pig(" ", 0, a);
+            Assert.Equal(expected, actual.Lard);
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(-1, 0)]
+        [InlineData(8, 8)]
+        public void TestAge(int a, int expected)
+        {
+            Pig actual = new Pig(" ", 0, a, 88);
+            Assert.Equal(expected, actual.Age);
+        }
+
+        [Theory]
+        [InlineData("Бычок", 5, "Из Бычок можно получить 5 сала\n")]
+        [InlineData("Бычок", 0, "Бычок Без сала\n")]
+        public void TestToString(string n, int a, string expected)
+        {
+            Bull actual = new Bull(n, 8, a);
+            Assert.Equal(expected, actual.ToString());
+        }
+    }
+}
---
+++ Tprogramming_2021-Nikita_Rybkin/CourseApp/Animals.cs
@@ -0,0 +1,117 @@
+﻿namespace CourseApp
+{
+    /* using System; */
+    /* using System.Collections.Generic;*/
+
+    public abstract class Animals
+    {
+        private int lard;
+        private int weight;
+        private int age;
+
+        public Animals()
+        : this("неизвестного животного", 10, 12, 13)
+        {
+        }
+
+        public Animals(string name, int weight)
+        : this(name, weight, 0, 0)
+        {
+        }
+
+        public Animals(string name, int weight, int lard)
+        : this(name, weight, 0, lard)
+        {
+        }
+
+        public Animals(string name, int weight, int age, int lard)
+        {
+            Name = name;
+            Lard = lard;
+            Age = age;
+            Weight = weight;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public int Weight
+        {
+            get
+            {
+                return weight;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    weight = 0;
+                }
+                else
+                {
+                    weight = value;
+                }
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    age = 0;
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+
+        public int Lard
+        {
+            get
+            {
+                return lard;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    lard = 0;
+                }
+                else
+                {
+                    lard = value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            if (lard == 0)
+            {
+                return $"{Name} Без сала\n";
+            }
+            else
+            {
+                return $"Из {Name} можно получить {Lard} сала\n";
+            }
+        }
+
+        public abstract string Died();
+
+        public abstract string MakePhrase(string[] phraseArray);
+    }
+}
